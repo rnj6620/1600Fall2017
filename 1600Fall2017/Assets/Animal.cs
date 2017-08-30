5894cdd7ed63574424b0bb30b7022e7945398164
Unknown <10795294@my.uvu.edu>
2017-08-30 10:05:44 -0600

    Changed Animals

---
+++ 1600Fall2017/Assets/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Die();
+	}
+	
+	void Die () {
+		print(this.name + " Dies");
+
+		
+	}
+}
---
+++ 1600Fall2017/Assets/Fish.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : Animal {
+
+	void Swim () {
+		print(this.name + " Swim");
+}
+
+	void Start()
+	{
+		Swim
