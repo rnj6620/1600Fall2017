7d9597f195f381a8e9886de06fb4e423e931869b
Unknown <10795294@my.uvu.edu>
2017-09-11 11:55:04 -0600

    Added if Statement

---
+++ 1600Fall2017/Assets/If.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class If : MonoBehaviour {
+
+	public Text input;
+	public string password = "Loser";
+
+	void Update () {
+		if(input.text == password) {
+			print("You know the password.");
+		}
+	}
+}
