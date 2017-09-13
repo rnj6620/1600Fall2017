6d42f536368677e3dd63ac28c170beb452ec0bcc
Unknown <10795294@my.uvu.edu>
2017-09-13 11:44:56 -0600

    Changed Else Clauses

--- 1600Fall2017/Assets/If.cs
+++ 1600Fall2017/Assets/If.cs
@@ -6,11 +6,21 @@ using UnityEngine.UI;
 public class If : MonoBehaviour {
 
 	public Text input;
+	public bool canPlayGame = false;
 	public string password = "Loser";
 
 	void Update () {
 		if(input.text == password) {
 			print("You know the password.");
+		 	canPlayGame = true;
+		}	else {
+			print("The password is incorrect.");
+		}
+
+		if(canPlayGame) {
+			print("Can Play Game.");
+		}	else {
+			print("Can't play game yet, Enter a correct password.");
 		}
 	}
 }
