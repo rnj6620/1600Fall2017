db4f01bcc3ad70146e6d5bdcd24e5564fac59d52
Unknown <10795294@my.uvu.edu>
2017-09-20 11:03:41 -0600

    Can't You tell I'm a Fangirl

---
+++ 1600Fall2017/Assets/YuuriandViktor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YuuriandViktor : MonoBehaviour {
+
+	
+	void Start () {
+		Couple();
+		Skate();
+		Engaged();
+	}
+	
+	
+	void Couple () {
+		print (this.name + " are the perfect couple");
+	}
+	void Skate () {
+		print (this.name + " skate together");
+	}
+	void Engaged () {
+		print (this.name + " are engaged");
+	}
+}
