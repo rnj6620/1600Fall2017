4b3aeebbcd6ffd5b152028a0f2402d4841e571c0
Unknown <10795294@my.uvu.edu>
2017-09-22 10:16:59 -0600

    Fangirl must fangirl

---
+++ 1600Fall2017/Assets/Makkachin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Makkachin : MonoBehaviour {
+
+	
+	void Start () {
+		Dog();
+		Cute();
+		SteamBuns();
+	}
+	
+	
+	void Dog () {
+		print (this.name + " is a puppy");
+	}
+	void Cute () {
+		print (this.name + " is very cute");
+	}
+	void SteamBuns (){
+		print (this.name + " nearly dies by choking on a steam bun");
+	}
+}
