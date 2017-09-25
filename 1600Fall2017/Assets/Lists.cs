d58b1bae64ff18cd19fc238381caf485deff3693
Unknown <10795294@my.uvu.edu>
2017-09-25 11:56:28 -0600

    Added Lists

---
+++ 1600Fall2017/Assets/Lists.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lists : MonoBehaviour 
+{
+	public List<GameObject> myWeapons;
+	
+	void OnTriggerEnter(Collider _weapon)
+	{
+		myWeapons.Add(_weapon.gameObject);
+	}
+}
