3042806a6ccdf51ee8715affff4ee98f5173b392
Unknown <10795294@my.uvu.edu>
2017-09-29 13:16:47 -0600

    Changed While Loops

--- 1600Fall2017/Assets/WhileLoops.cs
+++ 1600Fall2017/Assets/WhileLoops.cs
@@ -6,6 +6,14 @@ public class WhileLoops : MonoBehaviour {
 
 	int pizzaOnTheTable = 4;
 	int cookiesOnPlate = 10;
+	int bulletsInInventory = 2;
+	int mana = 40;
+	int enemyHealth = 90;
+	int weaponStrength = 100;
+	int armorStrength = 30;
+	int bread = 4;
+	int life = 10;
+	int warmth = 30;
 	// Use this for initialization
 	void Start () {
 		while(pizzaOnTheTable > 0)
@@ -18,6 +26,46 @@ public class WhileLoops : MonoBehaviour {
 		{
 			print("Cookies!");
 			cookiesOnPlate--;
+		}
+		while(bulletsInInventory < 100)
+		{
+			print("I need more bullets!");
+			bulletsInInventory++;
+		}
+		while(mana < 50)
+		{
+			print("My mana is low!");
+			mana++;
+		}
+		while(enemyHealth > 0)
+		{
+			print("We need to kill him!");
+			enemyHealth--;
+		}
+		while(weaponStrength > 30)
+		{
+			print("Don't fail me now!");
+			weaponStrength--;
+		}
+		while(armorStrength < 40)
+		{
+			print("My armor is Failing!");
+			armorStrength++;
+		}
+		while(bread > 0)
+		{
+			print("I guess I'll make a sandwich");
+			bread--;
+		}
+		while(life < 100)
+		{
+			print("I can't die like this!");
+			life++;
+		}
+		while(warmth < 90)
+		{
+			print("I will freeze!");
+			warmth++;
 		}
 	}
 	
