cf7ea61f6682359db2502b70d99a7fd808df99d4
Unknown <10795294@my.uvu.edu>
2017-09-29 11:11:06 -0600

    Created While Loops

---
+++ 1600Fall2017/Assets/WhileLoops.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileLoops : MonoBehaviour {
+
+	int pizzaOnTheTable = 4;
+	// Use this for initialization
+	void Start () {
+		while(pizzaOnTheTable > 0)
+		{
+			print("I think I'll have another slice!");
+			pizzaOnTheTable--;
+		}
+	}
+	
+	
+}
