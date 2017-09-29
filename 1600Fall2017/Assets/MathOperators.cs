67eca3255c53a022afbf52ea61e43e5d2cab25b5
Unknown <10795294@my.uvu.edu>
2017-09-29 11:46:30 -0600

    Imported Level

--- 1600Fall2017/Assets/MathOperators.cs
+++ 1600Fall2017/Assets/MathOperators.cs
@@ -6,25 +6,24 @@ public class MathOperators : MonoBehaviour {
 
 	// Use this for initialization
 	public float num1;
-	public float num2;	
+	public float num2;
 
 	public float addResult;
 	public float subResult;
 	public float multResult;
 	public float divResult;
 	public float remainderResult;
-	
-	
+
 	// Update is called once per frame
 	void Update () {
 		addResult = num1 + num2;
 		subResult = num1 - num2;
 		multResult = num1 * num2;
 		if(num1 != 0 && num2 != 0) {
-			divResult = num1 / num2;
+			divResult = num1 / num2;	
 		} else {
 			print("Can't Divide By 0.");
-		remainderResult = num1 % num2;
 		}
+		remainderResult = num1 % num2;
 	}
 }
--- 1600Fall2017/Assets/Vars.cs
+++ 1600Fall2017/Assets/Vars.cs
@@ -1,17 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
 public class Vars : MonoBehaviour {
-
 	public float health = 100;
-
-	
-	
 	void OnTriggerEnter()
 	{
 		health += 20;
 	}
-
-
 }
