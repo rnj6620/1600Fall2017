0be509c94d3b157aaea4d88995d85e8e0470e734
Unknown <10795294@my.uvu.edu>
2017-09-29 10:31:28 -0600

    Created RPG Weapon Switch

---
+++ 1600Fall2017/Assets/RPGWeaponSwitch.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RPGWeaponSwitch : MonoBehaviour {
+
+	public string [] weapons;
+	
+	public void MyWeaponSwitch (int i) {
+		switch (weapons[i])
+		{
+			case "Katana":
+				print("Katana");
+				break;
+			case "Sword":
+				print("Sword");
+				break;
+			case "GreatSword":
+				print("Great Sword");
+				break;
+			case "Axe":
+				print("Axe");
+				break;
+			case "GreatAxe":
+				print("Great Axe");
+				break;
+			case "Bow":
+				print("Bow");
+				break;
+			case "LongBow":
+				print("Long Bow");
+				break;
+			case "Dagger":
+				print("Dagger");
+				break;
+			case "Crossbow":
+				print("Crossbow");
+				break;
+			case "Chakram":
+				print("Chakram");
+				break;
+			default:
+				print("You're about to fight this enemy barehanded!");
+				break;
+		}
+	}
+}
