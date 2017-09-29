6270037d60e50c292b00f71d38f965c9ee3f07ec
Unknown <10795294@my.uvu.edu>
2017-09-29 11:34:34 -0600

    Changed Loops

--- 1600Fall2017/Assets/WhileLoops.cs
+++ 1600Fall2017/Assets/WhileLoops.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class WhileLoops : MonoBehaviour {
 
 	int pizzaOnTheTable = 4;
+	int cookiesOnPlate = 10;
 	// Use this for initialization
 	void Start () {
 		while(pizzaOnTheTable > 0)
 		{
 			print("I think I'll have another slice!");
 			pizzaOnTheTable--;
+			//Makes sure Unity doesn't crash
+		}
+		while(cookiesOnPlate > 0)
+		{
+			print("Cookies!");
+			cookiesOnPlate--;
 		}
 	}
 	
