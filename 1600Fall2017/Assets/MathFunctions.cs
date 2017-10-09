24c12e18cf5956afc429f2bab18236aedd20fc4a
Unknown <10795294@my.uvu.edu>
2017-10-09 11:47:16 -0600

    Added Functions
    I feel so stupid

---
+++ 1600Fall2017/Assets/MathFunctions.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathFunctions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		myScore = ReturnNum (health, ammo);
+		myPassword = ReturnPassword(myPassword);
+		myUserName = ReturnUser(myUserName);
+	}
+	
+	public int myScore = 100;
+	public int health;
+	public int ammo;
+
+	public string myPassword = "CoolStuff";
+	public string myUserName = "Noctis";
+
+	int ReturnNum (int _num1, int _num2) {
+		return _num1 + _num2;
+	}
+
+	string ReturnPassword (string _login) {
+		if(_login == "ou812") {
+		return "Password Correct";
+		} else {
+			return "Incorrect";
+		}
+		}
+	string ReturnUser (string _name) {
+		if(_name == "Prompto") {
+		return "Welcome Prompto";
+		} else {
+			return "Who are You?";
+		}
+	}
+}
