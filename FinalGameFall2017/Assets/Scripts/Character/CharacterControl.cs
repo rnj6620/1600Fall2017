8a5c7bf17e36401a9f597066e0562a3deb5d734f
Unknown <10795294@my.uvu.edu>
2017-11-15 11:49:17 -0700

    Changed Game

---
+++ FinalGameFall2017/Assets/Scripts/Character/CharacterControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour {
+
+	public CharacterController characterController;
+	public static bool gameOver = false;
+	public float gravity = 9.81f; // creates a gravity component, defaults to 9.81
+	public float speed = 10;
+	public float jumpForce = 50; // creates jump 
+	public Vector3 moveVector3; // temp variable that will store temp data
+
+	void Update () {
+		moveVector3.y -= gravity * Time.deltaTime; // will slow this down and make it coorelate with time
+
+		if(characterController.isGrounded && !gameOver){ // looks to see if character is on ground first
+			moveVector3.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime; // if so, can move
+
+			if(Input.GetKeyDown(KeyCode.Space)){ // if its on ground, can jump
+				moveVector3.y = jumpForce * Time.deltaTime; // creates jumpforce (shoots up)
+			}
+		}
+	
+		characterController.Move(moveVector3);
+
+	}
+}
