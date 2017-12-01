6d5aadfe40323a6185abe981f8dc2c90ccd384c9
Unknown <10795294@my.uvu.edu>
2017-12-01 11:55:59 -0700

    Added material

---
+++ FinalGameFall2017/Assets/Scripts/UIBar.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBar : MonoBehaviour {
+
+    public Image bar;
+    public Text endGameText;
+    public Text coinNum;
+    public int totalCoinValue;
+
+
+    public GameObject gameOverUI;
+    public float powerLevel = 0.1f;
+    public float amountToAdd = 0.0f;
+ public enum PowerUpType {
+        PowerUp,
+        PowerDown,
+        Win
+    }
+    public PowerUpType powerUp;
+	void OnTriggerEnter () {
+        switch (powerUp)
+        {
+            case PowerUpType.PowerUp:
+                StartCoroutine(PowerUpBar());
+            break;
+
+            case PowerUpType.PowerDown:
+                StartCoroutine(PowerDownBar());
+                break;
+                case PowerUpType.Win:
+                    EndGame("You Win!");
+                break;
+        }
+	}
+	  IEnumerator PowerUpBar () {
+        float tempAmount = bar.fillAmount + powerLevel;
+		if (tempAmount > 1) {
+			tempAmount = 1;
+		}
+        while (bar.fillAmount < tempAmount) {
+            bar.fillAmount += amountToAdd;
+            yield return new WaitForSeconds(amountToAdd);
+        }
+    }
+    IEnumerator PowerDownBar() {
+        float tempAmount = bar.fillAmount - powerLevel;
+       if(tempAmount < 0) {
+	   	tempAmount = 0;
+	   }
+	    while (bar.fillAmount > tempAmount) {
+            bar.fillAmount += amountToAdd;
+            yield return new WaitForSeconds(amountToAdd);
+
+            if (bar.fillAmount == 0) {
+              EndGame("Game Over");
+            }
+        }
+    }
+    void EndGame (string _text) {
+        endGameText.text = _text;
+        gameOverUI.SetActive(true);
+        CharacterControl.gameOver = true;
+    }
+}
